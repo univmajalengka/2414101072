82b5e74cc9c8a1d0d947ccb9e82743ba488c412f
ranggafirdauspazri <ranggafirdauspazri53@gmail.com>
2025-12-25 18:16:38 +0700

    Update diskon.php

--- tugas_2/diskon.php
+++ tugas_2/diskon.php
@@ -1,7 +1,4 @@
 <?php
-// -------------------------------
-// Fungsi hitungDiskon()
-// -------------------------------
 function hitungDiskon($totalBelanja) {
     $diskon = 0;
 
@@ -13,17 +10,13 @@ function hitungDiskon($totalBelanja) {
     return $diskon; // Mengembalikan nominal diskon
 }
 
-// -------------------------------
-// Contoh pemanggilan fungsi
-// -------------------------------
 $totalBelanja = 120000;
 $diskon = hitungDiskon($totalBelanja);
 $totalBayar = $totalBelanja - $diskon;
 
-// -------------------------------
-// Output
-// -------------------------------
+
 echo "Total Belanja : Rp. " . number_format($totalBelanja, 0, ',', '.') . "<br>";
 echo "Diskon        : Rp. " . number_format($diskon, 0, ',', '.') . "<br>";
 echo "Total Bayar   : Rp. " . number_format($totalBayar, 0, ',', '.') . "<br>";
+
 ?>
